56c899ca246cfcd4c609220bddf7f2f8e43e6d62
yavir13 <yav.castro@gmail.com>
2017-06-11 23:24:35 -0500

    Iteración 2, 3, 4
    Tarea 2, 3 y 4 del blog del curso

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  validates_presence_of :title
+  validates_presence_of :body
 end
